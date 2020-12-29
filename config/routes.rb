f8a206c95b501af35aa39deab9b14f335122ea9b
Matheus <matheussf.sc@gmail.com>
2020-12-29 13:31:26 -0300

    Init project: Users and Devise authentication

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    unauthenticated :user do
+      root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+end
