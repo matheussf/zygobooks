33e46ea549b50047b1b4c91958cfaaa1296f6d49
Matheus <matheussf.sc@gmail.com>
2020-12-29 19:19:22 -0300

    Gemfile: added shoulda-matchers

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context 'valid' do
+      it 'create by factory' do
+        expect(create(:blog)).to be_valid
+      end
+    end
+
+    context 'validations' do
+      it { should validate_presence_of(:url) }
+      it { should validate_presence_of(:title) }
+      it { should validate_presence_of(:resume) }
+      it { should validate_presence_of(:text) }
+      it { should validate_uniqueness_of(:url) }
+      it { should validate_uniqueness_of(:title) }
+    end
+
 end
